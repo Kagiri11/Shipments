f13ed99dd2a423156dfa7f8931b8d35f8c1cfe8b
Kagiri <charlesmaish444@gmail.com>
2025-05-25 11:47:01 +0300

    Add screen slide navigation animations

--- app/src/main/java/com/cmaina/shipments/ui/navigation/ShipmentNavigation.kt
+++ app/src/main/java/com/cmaina/shipments/ui/navigation/ShipmentNavigation.kt
@@ -1,5 +1,8 @@
 package com.cmaina.shipments.ui.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -11,6 +14,8 @@ import com.cmaina.shipments.ui.navigation.Screen.History
 import com.cmaina.shipments.ui.navigation.Screen.Home
 import com.cmaina.shipments.ui.navigation.Screen.Profile
 import com.cmaina.shipments.ui.navigation.Screen.Success
+import com.cmaina.shipments.ui.navigation.animation.slideIn
+import com.cmaina.shipments.ui.navigation.animation.slideOut
 import com.cmaina.shipments.ui.screens.calculate.CalculateScreen
 import com.cmaina.shipments.ui.screens.history.ShipmentHistoryScreen
 import com.cmaina.shipments.ui.screens.home.HomeScreen
@@ -23,21 +28,113 @@ fun ShipmentsNavigation(
     modifier: Modifier
 ){
     NavHost(modifier = modifier, navController = navController, startDestination = Home) {
-        composable<Home> {
+        composable<Home>(
+            enterTransition = {
+                when (initialState.destination.route) {
+                    in listOf(Calculate, History, Profile).map { it::class.qualifiedName } -> slideIn(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End
+                    )
+
+                    else -> EnterTransition.None
+                }
+            },
+            exitTransition = {
+                when (targetState.destination.route) {
+                    in listOf(Calculate, History, Profile).map { it::class.qualifiedName } -> slideOut(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start
+                    )
+
+                    else -> ExitTransition.None
+                }
+            }
+        ) {
             HomeScreen()
         }
 
-        composable<Profile> {
+        composable<Profile>(
+            enterTransition = {
+                when (initialState.destination.route) {
+                    in listOf(Calculate, History, Home).map { it::class.qualifiedName } -> slideIn(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start
+                    )
+
+                    else -> EnterTransition.None
+                }
+            },
+            exitTransition = {
+                when (targetState.destination.route) {
+                    in listOf(Calculate, History, Home).map { it::class.qualifiedName } -> slideOut(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End
+                    )
+
+                    else -> ExitTransition.None
+                }
+            }
+        ) {
             ProfileScreen()
         }
 
-        composable<Calculate> {
+        composable<Calculate>(
+            enterTransition = {
+                when (initialState.destination.route) {
+                    Home::class.qualifiedName -> slideIn(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start
+                    )
+
+                    in listOf(History, Profile).map { it::class.qualifiedName }-> slideIn(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End
+                    )
+
+                    else -> EnterTransition.None
+                }
+            },
+            exitTransition = {
+                when (targetState.destination.route) {
+                    Home::class.qualifiedName -> slideOut(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End
+                    )
+
+                    in listOf(History, Profile).map { it::class.qualifiedName } -> slideOut(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start
+                    )
+
+                    else -> ExitTransition.None
+                }
+            }
+        ) {
             CalculateScreen(
                 onNavigateBack = { navController.popBackStack() }
             )
         }
 
-        composable<History> {
+        composable<History>(
+            enterTransition = {
+                when (initialState.destination.route) {
+                    in listOf(Calculate, Home).map { it::class.qualifiedName } -> slideIn(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start
+                    )
+
+                    Profile::class.qualifiedName -> slideIn(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End
+                    )
+
+                    else -> EnterTransition.None
+                }
+            },
+            exitTransition = {
+                when (targetState.destination.route) {
+                    in listOf(Calculate, Home).map { it::class.qualifiedName } -> slideOut(
+                        towards = AnimatedContentTransitionScope.SlideDirection.End
+                    )
+
+                    Profile::class.qualifiedName -> slideOut(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Start
+                    )
+
+                    else -> ExitTransition.None
+                }
+            }
+        ) {
             ShipmentHistoryScreen(
                 onNavigateBack = { navController.popBackStack() }
             )
---
+++ app/src/main/java/com/cmaina/shipments/ui/navigation/animation/SlideAnimation.kt
@@ -0,0 +1,34 @@
+package com.cmaina.shipments.ui.navigation.animation
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.navigation.NavBackStackEntry
+
+const val animationDuration = 300
+fun AnimatedContentTransitionScope<NavBackStackEntry>.slideIn(towards: AnimatedContentTransitionScope.SlideDirection): EnterTransition {
+    return fadeIn(
+        animationSpec = tween(
+            animationDuration, easing = LinearEasing
+        )
+    ) + slideIntoContainer(
+        animationSpec = tween(animationDuration, easing = EaseIn),
+        towards = towards
+    )
+}
+
+fun AnimatedContentTransitionScope<NavBackStackEntry>.slideOut(towards: AnimatedContentTransitionScope.SlideDirection): ExitTransition {
+    return fadeOut(
+        animationSpec = tween(
+            animationDuration, easing = LinearEasing
+        )
+    ) + slideOutOfContainer(
+        animationSpec = tween(animationDuration, easing = EaseIn),
+        towards = towards
+    )
+}
